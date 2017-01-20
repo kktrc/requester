a2d3451973d484e9cc80642f52a6463b25ef8490
jerry <jerry.mo@fulaan.com>
2017-01-20 16:40:34 +0800

    init 2

--- src/main/java/com/mosl/requester/App.java
+++ src/main/java/com/mosl/requester/App.java
@@ -1,13 +1,17 @@
 package com.mosl.requester;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.info("hhahahh");
     }
 }
